5524517aaf99178d4b9ec4ce8b123228ed8664c1
PercyGFX <isurangabtk@gmail.com>
2023-06-01 20:45:32 +0530

    login controller completed

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -1,14 +1,70 @@
 const express = require('express');
 const axios = require('axios');
-const {connection} = require('../database')
+const connection = require('../database')
+const session = require('express-session')
+const { v4: uuidv4 } = require('uuid');
 
 
 const login = (req , res) =>{
 
+    if (req.session.phone) {
+        res.json({ success: true, message: 'already logged in' });
+        return;
+    }
+
     const phone = req.body.phone;
     const token = req.body.token;
+    const api_key = 'AIzaSyCCifumMfBcy8ugxcED55JZdDcuSh35HgM';
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${api_key}`;
+    const requestData = {
+        idToken: token,
+    };
 
-    res.send(phone + token)
+
+    axios
+        .post(url, requestData)
+        .then((response) => {
+
+            if (response.data.users[0].phoneNumber == phone) {
+
+                console.log('Phone number matches');
+
+                const q = "SELECT * from user WHERE phonenumber = ?"
+                connection.execute(q , [phone] , (err,result)=>{
+
+                    if(err){
+                        res.status(401).json({ success: false, message: 'Something went wrong' });
+                    }
+
+                    if(result.length < 1) {
+
+                        const userId = uuidv4();
+                        const q = "INSERT INTO user (uuid, phonenumber, userbalance) VALUES (?, ?, ?)";
+                        const values = [userId, phone, 0];
+
+                        connection.execute(q, values, (err, result)=>{
+                            if (err) {
+                                res.status(400).json(err);
+                            } else {
+                                console.log('new user added');
+                                req.session.phone = phone
+                                res.status(200).json({ message: "Login successful", user: phone, uuid: userId });
+                            }
+                        });
+                    } else {
+                        console.log(result);
+                        res.status(200).json({ message: "Login successful", data: result});
+                    }
+
+                })
+
+            } else {
+                res.status(401).json({ success: false, message: 'Something went wrong' });
+            }
+        })
+        .catch((error) => {
+            res.status(401).json({ success: false, message: 'Something wrong with phone number & Token' });
+        });
 }
 
 module.exports = login
