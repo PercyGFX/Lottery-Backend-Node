c631d6fb4d4eb6f932c8914ca15f1fe1c4444bcd
PercyGFX <isurangabtk@gmail.com>
2023-06-06 16:01:02 +0530

    added new time api

--- controllers/datecontroller.js
+++ controllers/datecontroller.js
@@ -3,29 +3,29 @@ const axios = require('axios');
 
 async function getSriLankaDateTime() {
     try {
-        const response = await axios.get('http://worldtimeapi.org/api/timezone/Asia/Colombo');
-        const { datetime } = response.data;
+        const response = await axios.get('http://api.timezonedb.com/v2.1/get-time-zone', {
+            params: {
+                key: '2VF6AOIGN3ZC',
+                format: 'json',
+                by: 'zone',
+                zone: 'Asia/Colombo'
+            }
+        });
 
-        const serverDateTime = new Date(datetime);
+        const { formatted, timestamp } = response.data;
+
+        const serverDateTime = new Date(formatted);
 
         const date = serverDateTime.toLocaleDateString('en-US');
         const time = serverDateTime.toLocaleTimeString('en-US', { timeZone: 'Asia/Colombo', hour12: false });
-        const timestamp = serverDateTime.getTime();
 
-        const year = serverDateTime.getFullYear();
-        const month = ('0' + (serverDateTime.getMonth() + 1)).slice(-2);
-        const day = ('0' + serverDateTime.getDate()).slice(-2);
-        const hours = ('0' + serverDateTime.getHours()).slice(-2);
-        const minutes = ('0' + serverDateTime.getMinutes()).slice(-2);
-        const seconds = ('0' + serverDateTime.getSeconds()).slice(-2);
-
-        const mysqlTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        const mysqlTimestamp = formatted;
 
         return { date, time, timestamp, mysqlTimestamp };
     } catch (error) {
         console.error('Error retrieving Sri Lanka date, time, and timestamp:', error.message);
         return null;
     }
-}
+} 
 
 module.exports = getSriLankaDateTime
