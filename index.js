3a324565c9ac30db9ed3a3f4876f0d830929a091
PercyGFX <isurangabtk@gmail.com>
2023-06-07 15:55:18 +0530

    port fix

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.use('/', login);
 app.use('/', bookslot)
 
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000');
-});
+const port = process.env.PORT || 3000; // Default to port 3000 if PORT environment variable is not set
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
