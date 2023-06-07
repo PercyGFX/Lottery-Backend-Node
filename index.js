3396e9a2374bdf14aed5ee80317847e5743b9220
PercyGFX <isurangabtk@gmail.com>
2023-06-07 19:31:20 +0530

    chrome issue

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use(   session({
-    store: store,
-    secret: process.env.SESSION_SECRET,
+    secret: 'damnthisissosecret',
     resave: false,
     saveUninitialized: true,
     proxy: true,
