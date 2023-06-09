063cb86fe352033a522cdaa36f4dbcd842832d4a
PercyGFX <isurangabtk@gmail.com>
2023-06-09 14:01:24 +0530

    front end change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 
 
 app.use(cors({
-    origin: 'https://lottery-1rcs-oo8a2mvda-percygfx.vercel.app',
+    origin: 'https://lottery-1rcs.vercel.app/',
     credentials: true
 }));
 
